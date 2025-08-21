f884000148f30f3c302d03f856ca79b926b7180b
minori Koshikawa <greenboy08031115@gmail.com>
2025-08-21 19:56:09 +0900

    マイグレーション・シーディング・モデル作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /*
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Contact::factory(35)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    $this->call(CategoriesTableSeeder::class);
+  }
 }
