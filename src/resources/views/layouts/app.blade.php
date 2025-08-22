85973d27cf5821948612683c34476c06197eaa40
minori Koshikawa <greenboy08031115@gmail.com>
2025-08-22 12:52:56 +0900

    お問い合わせフォーム入力画面の作成完了

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!--あとでＣＳＳ読みこみ-->
+
+
+
+  <title>FashionablyLate</title>
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <a class="header__logo">
+        FashionablyLate
+      </a>
+    </div>
+  </header>
+
+  <main>
+    @yield('content')
+  </main>
+</body>
+</html>
