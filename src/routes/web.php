64667082b9e7fd96d3d23a3cfeed796d03503585
minori Koshikawa <greenboy08031115@gmail.com>
2025-08-22 09:07:12 +0900

    コントローラーとルーティング追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [ContactController::class, 'index']);
+
+Route::post('/confirm', [ContactController::class, 'confirm']);
+
+Route::post('/thanks', [ContactController::class, 'thanks']);
+
