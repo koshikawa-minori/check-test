6eeaac55413292fc5b98fbeb13a1c1291aac3aa9
minori Koshikawa <greenboy08031115@gmail.com>
2025-08-22 16:58:23 +0900

    サンクスページ完了

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>FashionablyLate</title>
+</head>
+
+<body>
+
+<!--CSS書く-->
+
+<main>
+    <div class="thanks__content">
+      <div class="thanks__content--group">
+        <p>Thank you</p>
+        <div class="thanks__head">
+          <h2>お問い合わせありがとうございました</h2>
+        </div>
+        <div class="thanks__button">
+          <a class="thanks__button-submit" href="/">HOME</a>
+        </div>
+      </div>
+    </div>
+</main>
+</body>
+</html>
